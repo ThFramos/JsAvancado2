6e3f8aecb2193b66ebe931a21569f4b196350abd
Thiago Faria Ramos <62444689+ThFramos@users.noreply.github.com>
2021-01-27 22:37:51 -0300

    aula1

--- jsexpert02-skeleton-ew/pages/room/src/business.js
+++ jsexpert02-skeleton-ew/pages/room/src/business.js
@@ -1,9 +1,15 @@
 class Business {
-  constructor({ room, media, view }) {
+  constructor({ room, media, view, socketBuilder }) {
     this.room = room;
     this.media = media;
     this.view = view;
 
+    this.socketBuilder = socketBuilder
+      .setOnUserConnected(this.onUserConnected())
+      .setOnUserDisconnected(this.onUserDisconnected())
+      .build();
+
+    this.socketBuilder.emit('join-room', this.room, 'teste01')
     this.currentStream = {}
   }
   static initialize(deps) {
@@ -23,5 +29,18 @@ class Business {
       stream,
       isCurrentId
     })
+  }
+
+  onUserConnected = function () {
+
+    return userId => {
+      console.log('user Connected!', userId)
+    }
+  }
+  onUserDisconnected = function () {
+
+    return userId => {
+      console.log('user Disconneted!', userId)
+    }
   }
 }
